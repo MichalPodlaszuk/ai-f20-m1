b326ea58c263074045469d64ae815839e6fe5298
michalpodlaszuk <michalpodlaszuk@gmail.com>
2020-12-09 16:00:56 +0100

    webscrapping

---
+++ w2/d7/pans.py
@@ -0,0 +1,23 @@
+u = []
+for a in range(1, 10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(10):
+                for e in range(10):
+                    for f in range(10):
+                        for g in range(10):
+                            for h in range(10):
+                                for i in range(10):
+                                    for j in range(10):
+                                        for k in range(10):
+                                            for l in range(10):
+                                                for m in range(10):
+                                                    for n in range(10):
+                                                        for o in range(10):
+                                                            for p in range(10):
+                                                                for q in range(10):
+                                                                    for r in range(10):
+                                                                        for s in range(10):
+                                                                            for t in range(10):
+                                                                                u.append(f'{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}{m}{n}{o}{p}{q}{r}{s}{t}')
+print(u)
