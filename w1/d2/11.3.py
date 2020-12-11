1fdf4724ff14e2135d4062ef40cf32fcc9c14329
michalpodlaszuk <michalpodlaszuk@gmail.com>
2020-12-11 16:50:29 +0100

    Misery

--- w1/d2/11.3.py
+++ w1/d2/11.3.py
@@ -1,6 +1,13 @@
-fp = open('blakepoems.txt', 'r')
-fp1 = fp.readlines()
-fp2 = []
-for line in fp1:
-    fp2.append(line.lower().replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', ''))
-print(fp2)
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+main_page = requests.get('https://www.imdb.com/chart/top/')
+main = BeautifulSoup(main_page.content, 'html.parser')
+titles = main.find_all('a')
+list_titles = list(titles)
+proper_elements = list_titles[60:259:2]
+href = []
+
+test = requests.get('https://www.imdb.com/title/tt0111161/')
+test1 = BeautifulSoup(test.content, 'html.parser')
+print(test1.find_all('div'))
